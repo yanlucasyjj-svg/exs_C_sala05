6df744db25a39fa1daf9468e4cef0b1242186db3
yanlucasyjj-svg <yan.lucas.yjj@gmail.com>
2026-06-11 16:59:57 -0300

    Add files via upload

--- C-Sala05-extra/ex3.c
+++ C-Sala05-extra/ex3.c
@@ -1,9 +1,16 @@
-#include <stdio.h>
+#if defined(__has_include)
+#  if __has_include(<stdio.h>)
+#    include <stdio.h>
+#  else
+int printf(const char *, ...);
+#  endif
+#else
+#  include <stdio.h>
+#endif
 
 int main(void) {
 
-
- int i;
+    int i;
 
     for (i = 1; i <= 10; i++) {
         printf("9 x %d = %d\n", i, 9 * i);
