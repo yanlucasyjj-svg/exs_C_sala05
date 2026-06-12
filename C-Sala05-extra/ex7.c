4c4bbffcb3f34feba36726368fe2b4c46e7f2beb
yan-yjj <yan.lucas.yjj@gmail.com>
2026-06-12 15:31:06 -0300

    Add files via upload

---
+++ C-Sala05-extra/ex7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    float peso, altura, imc;
+
+    printf("Digite o peso (kg): ");
+    scanf("%f", &peso);
+
+    printf("Digite a altura (m): ");
+    scanf("%f", &altura);
+
+    imc = peso / (altura * altura);
+
+    printf("IMC = %.2f\n", imc);
+
+    return 0;
+}
